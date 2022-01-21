3293286e4806586b86c5b80faa3130fb73df11bb
princepf <ss.princef@gmail.com>
2022-01-21 08:23:35 +0530

    commit

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
 
 class HomeScreen extends StatefulWidget {
   HomeScreen({Key? key}) : super(key: key);
@@ -11,32 +12,48 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
+      backgroundColor: Color(0xFF035AA6),
       appBar: AppBar(
-        backgroundColor: Colors.white,
+        backgroundColor: Color(0xFF035AA6),
         elevation: 0,
-        leading: Icon(
-          Icons.arrow_back,
-          size: 30,
-          color: Colors.black,
-        ),
-        actions: [
-          Icon(
-            Icons.search,
-            size: 30,
-            color: Colors.black,
-          ),
-          SizedBox(
-            width: 7,
-          ),
-          Icon(
-            Icons.shopping_cart_outlined,
-            size: 30,
-            color: Colors.black,
+        title: Text("Dashbord"),
+        actions: <Widget>[
+          IconButton(
+            icon: SvgPicture.asset(
+              "assets/icons/notification.svg",
+              height: 20,
+            ),
+            onPressed: () {},
           ),
           SizedBox(
             width: 10,
           ),
+        ],
+      ),
+      body: ListView(
+        children: [
+          //search
+          Container(
+              margin: EdgeInsets.all(20),
+              padding: EdgeInsets.symmetric(horizontal: 20),
+              height: 50,
+              width: double.infinity,
+              decoration: BoxDecoration(
+                  color: Colors.grey.withOpacity(0.7),
+                  borderRadius: BorderRadius.circular(15)),
+              child: TextFormField(
+                decoration: InputDecoration(
+                  enabledBorder: InputBorder.none,
+                  focusedBorder: InputBorder.none,
+                  icon: Icon(
+                    Icons.search_rounded,
+                    color: Colors.white,
+                    size: 30,
+                  ),
+                  hintText: 'search',
+                  hintStyle: TextStyle(fontSize: 18, color: Colors.white),
+                ),
+              ))
         ],
       ),
     );
